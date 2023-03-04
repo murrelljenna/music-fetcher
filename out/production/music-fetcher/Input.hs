2e444945c664c64b86e2270ff3a2f1d5856a4897
Jenna Murrell <murrelljenna@gmail.com>
2023-03-03 21:35:25 -0500

    Parse candidates from music titles

--- out/production/music-fetcher/Input.hs
+++ out/production/music-fetcher/Input.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Input(parseCandidatesFromTitle, fetchFilenames) where
+module Input(parseCandidatesFromTitle, fetchFilenames, Title, Fragment, Candidate) where
 
 import Data.Text hiding (dropWhile, reverse, map, splitOn)
 import System.Directory
 import Data.List.Split
 import Data.Char (isSpace)
 
+type Title = String
+type Fragment = String
+type Candidate = (String, String)
 
 testDirPath :: FilePath
 testDirPath = "E:\\Music\\Electronic"
@@ -18,5 +21,16 @@ trim :: String -> String
 trim = f . f
           where f = reverse . dropWhile isSpace
 
-parseCandidatesFromTitle :: String -> [String]
-parseCandidatesFromTitle s = map (\s -> trim s) (splitOn "-" s)
+fragment :: Title -> [Fragment]
+fragment t = map (\s -> trim s) (splitOn "-" t)
+
+candidatesFromFragments :: [Fragment] -> [Candidate]
+candidatesFromFragments [] = []
+candidatesFromFragments [f1] = []
+candidatesFromFragments [f1, f2] = [(f1, f2), (f2, f1)]
+candidatesFromFragments [f1, f2, f3] = candidatesFromFragments [f1 <> f2, f3] ++ candidatesFromFragments [f1, f2 <> f3]
+candidatesFromFragments [f1, f2, f3, f4] = candidatesFromFragments [f1 <> f2, f3 <> f4] ++ candidatesFromFragments [f1 <> f2 <> f3, f4]
+candidatesFromFragments _ = []
+
+parseCandidatesFromTitle :: Title -> [Candidate]
+parseCandidatesFromTitle t = (candidatesFromFragments . fragment) t
--- test/InputSpec.hs
+++ test/InputSpec.hs
@@ -3,8 +3,19 @@ import Test.HUnit
 import Input
 
 parseCandidatesTest :: Test
-parseCandidatesTest = TestCase $ assertEqual "" ["Zoot Woman", "Grey Day"] (parseCandidatesFromTitle "Zoot Woman - Grey Day")
+parseCandidatesTest = TestCase $ assertEqual "All expected inputs match expected results" expectedResults (parseCandidatesFromTitle <$> expectedInputs)
 
 spec :: Test
 spec = TestList [parseCandidatesTest]
 
+fixture :: [(Title, [Candidate])]
+fixture = [
+  ("Zoot Woman - Grey Day",[("Zoot Woman", "Grey Day"), ("Grey Day", "Zoot Woman")])
+  , ("Autechre - Montreal",[("Autechre", "Montreal"), ("Montreal", "Autechre")])
+  ]
+
+expectedInputs :: [Title]
+expectedInputs = fst <$> fixture
+
+expectedResults :: [[Candidate]]
+expectedResults = snd <$> fixture
